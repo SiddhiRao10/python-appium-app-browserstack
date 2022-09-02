e6e83d9d08d8e561efdee1062bb5852e09562c0e
SiddhiRao10 <107387552+SiddhiRao10@users.noreply.github.com>
2022-09-02 19:04:55 +0530

    Update browserstack_sample.py

--- android/browserstack_sample.py
+++ android/browserstack_sample.py
@@ -7,8 +7,8 @@ import os
 
 desired_cap = {
     # Set your access credentials
-    "browserstack.user" : os.environ['BROWSERSTACK_USERNAMEC'],
-    "browserstack.key" : os.environ['BROWSERSTACK_ACCESS_KEYC'],
+    "browserstack.user" : os.environ['BROWSERSTACK_USERNAME'],
+    "browserstack.key" : os.environ['BROWSERSTACK_ACCESS_KEY'],
 
     # Set URL of the application under test
     "app" : os.environ['BROWSERSTACK_APP_URL'],
